3a7c5f0dddf0995a5bd85a1ea28b941b98e278ce
David <david.perez@grupomeiko.com>
2020-10-20 16:32:38 -0500

    imagenes

---
+++ controllers/uploads.js
@@ -0,0 +1,72 @@
+const { response } = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+const fileUpload = ( req, res = response ) => {
+
+    const tipo = req.params.tipo;
+    const id = req.params.id;
+
+    // Validar tipo
+    const tiposValidos = ['hospitales', 'medicos', 'usuarios'];
+    console.log(tipo);
+
+    if(!tiposValidos.includes(tipo)){
+        return res.status(400).json({
+            ok: false,
+            msg: 'No es un médico, usuario u hospital (tipo)'
+        });
+    }
+
+
+    // Validar que exista un archivo
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({
+            ok: false, 
+            msg: 'No hay ningun archivo.'
+        });
+    }
+
+    // Procesar la imagen...
+    const file = req.files.imagen;
+
+    const nombreCortado = file.name.split('.');
+    const extensionArchivo = nombreCortado[ nombreCortado.length - 1 ];
+
+    // Validar extensión
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+    if( !extensionesValidas.includes(extensionArchivo) ){
+        return res.status(400).json({
+            ok: false, 
+            msg: 'No es una extension permitida.'
+        });
+    }
+
+    // Generar nombre del archivo
+    const nombreArchivo = `${ uuidv4() }.${ extensionArchivo }`;
+
+    // Path para guardar la imagen
+    const path = `./uploads/${ tipo }/${ nombreArchivo }`;
+
+    // Mover la imagen
+    file.mv(path, (err) => {
+        if (err){
+            console.log(err);
+            return res.status(500).json({
+                ok: false,
+                msg: 'Error al mover la imagen'
+            });
+        }        
+
+        res.json({
+            ok: true,
+            msg: 'Archivo subido',
+            nombreArchivo
+        });
+    });
+
+}
+
+
+module.exports = {
+    fileUpload
+}
