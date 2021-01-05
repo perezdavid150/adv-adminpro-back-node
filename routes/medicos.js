8d15a372d5f3de38477f74dcd3fc1422f5d30d52
David Perez <dagui150@gmail.com>
2021-01-05 11:10:15 -0500

    servicio get medico by id

--- routes/medicos.js
+++ routes/medicos.js
@@ -12,12 +12,13 @@ const {
   getMedicos,
   crearMedico,
   actualizarMedico,
-  borrarMedico
+  borrarMedico,
+  getMedicoById
 } = require('../controllers/medicos')
 
 const router = Router();
 
-router.get( '/', getMedicos);
+router.get( '/', validarJWT, getMedicos);
 
 router.post( '/', 
   [
@@ -39,6 +40,8 @@ router.put( '/:id',
 );
 
 router.delete( '/:id', validarJWT, borrarMedico );
+
+router.get( '/:id', validarJWT, getMedicoById );
 
 
 
