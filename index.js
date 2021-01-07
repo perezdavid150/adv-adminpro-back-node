05c1f237fd1367e469e9d10ba711513f9ed3522a
David Perez <dagui150@gmail.com>
2021-01-07 11:06:55 -0500

    ajustes en ruta para SPA

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
 
 const cors = require('cors');
 
@@ -29,6 +30,10 @@ app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/upload', require('./routes/uploads') );
 
+// último
+app.get('*', (req, res) => {
+  res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+})
 
 app.listen( process.env.PORT, () => {
   console.log('Servidor corriendo en puerto ' + process.env.PORT);
